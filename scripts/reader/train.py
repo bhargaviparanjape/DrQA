acdea75b1123ce99211ac2982043b79e3525896a
bhargavi.paranajape <bhargavi22294@gmail.com>
2018-11-11 09:02:18 -0500

    elmo trainer

--- scripts/reader/train.py
+++ scripts/reader/train.py
@@ -18,10 +18,18 @@ import numpy as np
 import torch
 
 sys.path.insert(0, dirname(dirname(dirname(realpath(__file__)))))
-from drqa.reader import utils, config
-from drqa.reader import data as reader_data, vector as reader_vector
+
+# For GloVe based reader
+# from drqa.reader import utils, config
+# from drqa.reader import data as reader_data, vector as reader_vector
+# from drqa.selector import data as selector_data, vector as selector_vector
+# from drqa.reader import DocReader
+
+from drqa.elmo_reader import utils, config
+from drqa.elmo_reader import data as reader_data, vector as reader_vector
 from drqa.selector import data as selector_data, vector as selector_vector
-from drqa.reader import DocReader
+from drqa.elmo_reader import DocReader
+
 from drqa.selector import SentenceSelector
 from scripts.selector.train import validate_unofficial as validate_selector
 from drqa import DATA_DIR as DRQA_DATA
@@ -338,6 +346,12 @@ def validate_official(args, data_loader, model, global_stats,
                 e_offset = offsets[ex_id[i]][pred_e[i][0]][1]
             prediction = texts[ex_id[i]][s_offset:e_offset]
 
+            if args.select_k > 1:
+                prediction = ""
+                offset_subset = chosen_offset[i][pred_s[i][0]: pred_e[i][0]]
+                for enum_, o in enumerate(offset_subset):
+                    prediction += texts[ex_id[i]][o[0]:o[1]] + " "
+                prediction = prediction.strip()
             # Compute metrics
             ground_truths = answers[ex_id[i]]
             exact_match.update(utils.metric_max_over_ground_truths(
